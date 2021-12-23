e5f81b2edeea56c9d1ac9412fb847b3fc1a2ea22
Jean Junior <jclgjunior@gmail.com>
2021-12-23 14:43:04 -0300

    fix: test kafka producer

--- libs/KafkaProducer/src/KafkaProducer.service.spec.ts
+++ libs/KafkaProducer/src/KafkaProducer.service.spec.ts
@@ -1,3 +1,4 @@
+import { ClientKafka } from '@nestjs/microservices';
 import { Test, TestingModule } from '@nestjs/testing';
 import { KafkaProducerService } from './KafkaProducer.service';
 
@@ -6,7 +7,13 @@ describe('KafkaProducerService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [KafkaProducerService],
+      providers: [
+        KafkaProducerService,
+        {
+          provide: 'KAFKA_SERVICE',
+          useClass: ClientKafka,
+        },
+      ],
     }).compile();
 
     service = module.get<KafkaProducerService>(KafkaProducerService);
@@ -14,5 +21,17 @@ describe('KafkaProducerService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should emit message to topic', async () => {
+    const topic = 'test';
+    const message = {
+      id: '123',
+      name: 'test',
+    };
+
+    const result = await service.emit(topic, message);
+
+    expect(result).toEqual({ topic, message });
   });
 });
